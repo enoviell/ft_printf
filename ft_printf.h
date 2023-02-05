213ed96b2a7208097cf8c25ff5e5fbc4caa5996d
enoviell <123171897+enoviell@users.noreply.github.com>
2023-02-05 17:37:40 +0100

    Add files via upload

---
+++ ft_printf.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: enoviell <enoviell@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/05 16:37:59 by enoviell          #+#    #+#             */
+/*   Updated: 2023/02/05 17:10:19 by enoviell         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+#	define FT_PRINTF_H
+
+int	ft_putchar(char c);
+int	ft_printstr(char *s);
+int ft_putnbr(long int n);
+int ft_printnbr(long int n);
+int ft_printunsigned(unsigned int unsnbr);
+
+#endif
